4389aa570886f3d9aa51a3bd7a6717f443f4cfa3
Myles Bennett <myles.bennett@hotmail.com>
2019-03-22 17:23:14 -0400

    completed exercise

---
+++ pacman-arrays.js
@@ -0,0 +1,7 @@
+const ghosts = ['Inky', 'Blinky', 'Pinky', 'Clyde'];
+
+console.log(`There are ${ghosts.length} ghosts.`);
+console.log(ghosts[0]);
+console.log(ghosts[1]);
+console.log(ghosts[2]);
+console.log(ghosts[3]);
---
+++ pacman-if-else.js
@@ -0,0 +1,19 @@
+let powerPelletEaten = false;
+let ghosts = 4;
+
+while(true) {
+  console.log('Ghosts remaining: ' + ghosts);
+
+  if (powerPelletEaten == false) {
+    console.log('Pac-Man eats a ghost.');
+    powerPelletEaten = true;
+  } else if (ghosts > 0) {
+    console.log('Pac-Man eats a ghost.');
+    ghosts--;
+  } else {
+    break;
+  }
+}
+
+
+console.log('Pac-Man returns home after a long day in the maze')
